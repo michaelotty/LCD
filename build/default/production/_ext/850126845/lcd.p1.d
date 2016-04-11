a0e034de8ec31ee0b35a66b4a7c1d0350d12ed93
mo360 <mo360@EEPC-254.campus.bath.ac.uk>
2016-04-11 15:37:21 +0100

    initial

---
+++ build/default/production/_ext/850126845/lcd.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/_ext/850126845/lcd.d  \
+ build/default/production/_ext/850126845/lcd.p1:  \
+ //myfiles/mo360/dos/PIC/LCD.X/lcd.c  \
+//myfiles/mo360/dos/PIC/LCD.X/lcd.h 
--- main.c
+++ main.c
@@ -1,12 +1,32 @@
-#pragma     config  FOSC = XT
-#pragma     config  WDTE = OFF
-#pragma     config  PWRTE = ON
-#pragma     config  CP = OFF
+#pragma config  FOSC = XT
+#pragma config  WDTE = OFF
+#pragma config  PWRTE = ON
+#pragma config  CP = OFF
 
 #define _XTAL_FREQ  4000000
 
+#include "lcd.h"
 #include <xc.h>
 
-void int main() {
+
+void main() {
+    unsigned int counter = 0;
+    TRISB = 0x00;
+    //char *name = "Lauael";
     
+    RW = 0;
+    
+    Lcd_Init();
+    
+    while(1) {
+        //Lcd_Clear();
+        Lcd_Set_Cursor(1,1);
+        
+        //Lcd_Write_String(name);
+        
+        Lcd_Write_Int(counter);
+        __delay_ms(1000);
+        counter++;
+    }
+    return;
 }
