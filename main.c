69f43cee4b069bc2a815eb97a2a99ac88e297124
mo360 <mo360@EEPC-254.campus.bath.ac.uk>
2016-04-11 14:54:50 +0100

    initial

---
+++ main.c
@@ -0,0 +1,12 @@
+#pragma     config  FOSC = XT
+#pragma     config  WDTE = OFF
+#pragma     config  PWRTE = ON
+#pragma     config  CP = OFF
+
+#define _XTAL_FREQ  4000000
+
+#include <xc.h>
+
+void int main() {
+    
+}
