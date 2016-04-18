a129136786304a7b785095d7188a826037c8a2d6
mo360 <mo360@EEPC-267.campus.bath.ac.uk>
2016-04-18 15:32:22 +0100

    finish

--- main.c
+++ main.c
@@ -9,23 +9,24 @@
 
 #include "lcd.h"
 #include <xc.h>
+//#include <string.h>
 
-int SplitText(char Instruction[]) //Function to remove the first 8 chaacters of a string
-{
-    int i;
-    int k = 9;
-    char Instruction[] = "Push button to generate a random number";
-    char SecondPart[100]; //Another empty character array
-    char RightText[100];
-    for (int i = 0; i < 50; i++)
-    {
-        SecondPart[i] = Instruction[k];
-        k++;
-    }
-    strncpy(RightText, SecondPart, 50);
-    RightText[50] = "\0";
-    return RightText;
-}
+//char* SplitText(char Instruction[]) //Function to remove the first 8 chaacters of a string
+//{
+////    int i;
+//    int k = 9;
+////    char Instruction[] = "Push button to generate a random number";
+//    char SecondPart[16]; //Another empty character array
+//    char RightText[16];
+//    for (int i = 0; i < 15; i++)
+//    {
+//        SecondPart[i] = Instruction[k];
+//        k++;
+//    }
+//    strncpy(RightText, SecondPart, 15);
+//    RightText[15] = '\0';
+//    return RightText;
+//}
 
 void main() {
     TRISA = 0xFF;
@@ -33,29 +34,44 @@ void main() {
 
     RW = 0;
 
-    Lcd_Init();
-    int num = 1 + rand() % 6; //generate a random number between 1 & 6
+    Lcd_Init();; //generate a random number between 1 & 6
 
-    char Instruction[] = "Push button to generate a random number";
-    char LeftText[100]; //Emptry character array of size 100
-    strncpy(LeftText, Instruction, 50); //convert 'Instruction' into a string called 'LeftText'==!=
-    LeftText[50] = '\0';
-    char *RightText = SplitText(Instruction); //Call function 'SplitText' 
-
-    Lcd_Set_Cursor(1, 1); //First 8 parts of LCD
-    Lcd_Write_String(LeftText); 
-    Lcd_Set_Cursor(2, 1); //Seocnd 8 parts of LCDpp
-    Lcd_Write_String(RightText); p
-
-//need to write it so that it stays on the number until the button is pressed again...
-    while (1) {
+    char *Instruction =  "Push button to generate a random number \0";
+    char *Instruction2 = "ton to generate a random number Push but\0";
+    
+    Lcd_Set_Cursor(1,1);
+    Lcd_Write_String(Instruction);
+    Lcd_Set_Cursor(2,1);
+    Lcd_Write_String(Instruction2);
+    
+    int run = 1;
+    
+    while (run)
+    {
+        __delay_ms(500);
         Lcd_Shift_Left();
+        
         if(BUTTON) 
         {
-            num = 1 + rand() % 6;
+            run = 0;
+            int num = 1 + rand() % 6;
+            Lcd_Clear();
             Lcd_Write_Int(num);
+            __delay_ms(2000);
+            Lcd_Clear();
         }        
     }
+    
 }
 
-
+//    char LeftText[16]; //Empty character array of size 100
+//    strncpy(LeftText, Instruction, 15); //convert 'Instruction' into a string called 'LeftText'==!=
+//    LeftText[15] = '\0';
+//    char *RightText = SplitText(Instruction); //Call function 'SplitText' 
+//
+//    Lcd_Set_Cursor(1, 1); //First 8 parts of LCD
+//    Lcd_Write_String(LeftText); 
+//    Lcd_Set_Cursor(2, 1); //Second 8 parts of LCDpp
+//    Lcd_Write_String(RightText); 
+//
+////need to write it so that it stays on the number until the button is pressed again...
